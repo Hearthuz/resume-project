6d6e13407ee7fc8de1e23979b44fde125bf53b2e
Hearthuz <ariya.watcharaapanukorn@gmail.com>
2025-03-24 03:13:01 +0700

    first commit

---
+++ src/components/ItemsBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="details">
+      <h3>
+        <slot name="heading"></slot>
+      </h3>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.wrapper {
+  background-color: rgba(255, 255, 255, 0.5);
+  padding: 1rem;
+  border-radius: 1rem;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import ItemBox from './ItemsBox.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+
+import { ref } from 'vue'
+
+const show = ref(false)
+</script>
+
+<template>
+  <div>Technical Skills</div>
+  <button @click="show = true">Icons</button>
+  <button @click="show = false">List</button>
+  <Transition>
+    <div v-if="show">
+      <div class="wrapper">
+        <ItemBox> </ItemBox>
+      </div>
+      <WelcomeItem>
+        <template #icon>
+          <DocumentationIcon />
+        </template>
+        <template #heading>Documentation</template>
+
+        Vue’s
+        <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+        provides you with all information you need to get started.
+      </WelcomeItem>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
